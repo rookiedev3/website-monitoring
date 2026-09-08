ea4c269cd6c93400522082a4ca0425b29d05f39e
Zahwa Ayu Ramadhani <zahwaayu.stu@pnc.ac.id>
2026-09-08 15:05:41 +0700

    perbaikan notifikasi email

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard Monitoring Status Website</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="icon" type="image/png" href="{{ asset('img/logo.jpeg') }}">
 
   <!-- Fonts & Icons -->
